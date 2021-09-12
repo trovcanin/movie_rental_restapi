56de57d2573a58f8237d6d4fb25ce8748fa6c731
tar-ik <tarik.rovcan@gmail.com>
2021-09-12 11:31:49 +0200

    fixes

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const movieController = require('../controllers/movieController');
 const { loginAuth, verifyToken} = require('../authorization/authToken')
 
-router.get('/api/movie', movieController.get_all_movies);
+router.get('/api/movie',verifyToken, movieController.get_all_movies);
 router.post('/api/movie', verifyToken, movieController.post_new_movie);
-router.put('/movie/update/:id', movieController.update_movie);
-router.delete('/movie/:id', movieController.delete_movie);
+router.put('/movie/update/:id', verifyToken,movieController.update_movie);
+router.delete('/movie/:id', verifyToken,movieController.delete_movie);
 
 module.exports = router;
+
